a512f555efb4708ce4fa6f15043b615c870b12a3
Coud <nikielprzemek@gmail.com>
2019-03-03 00:05:21 +0100

    sftp

--- rpi_connect.sh
+++ rpi_connect.sh
@@ -14,17 +14,21 @@ function initialize_token {
     | jq -r '.token')
 }
 
-if [ "$1" == "localssh" ] ; then
-    xfce4-terminal -e "ssh $LINUXUSER@$LOCALIP"
-    elif [ "$1" == "remotessh" ] ; then
-    initialize_token
+function initialize_proxyaddress {
     PROXYADDRESS=($(curl -X POST \
             -H "token:$TOKEN" \
             -H "developerkey:$DEVELOPERKEY" \
             -d '{"wait":"true","deviceaddress":"'$SSHDEVICEADDRESS'"}' \
     https://api.remot3.it/apv/v27/device/connect | jq -r '.connection.proxyserver,.connection.proxyport'))
+}
+
+if [ "$1" == "localssh" ] ; then
+    xfce4-terminal -e "ssh $LINUXUSER@$LOCALIP"
+    elif [ "$1" == "remotessh" ] ; then
+    initialize_token
+    initialize_proxyaddress
     xfce4-terminal -e "ssh -l $LINUXUSER ${PROXYADDRESS[0]} -p ${PROXYADDRESS[1]}"
-else
+    elif [ "$1" == "remoteweb" ] ; then
     initialize_token
     WEB=$(curl -X POST \
         -H "token:$TOKEN" \
@@ -32,4 +36,12 @@ else
         -d '{"wait":"true","deviceaddress":"'$WEBDEVICEADDRESS'"}' \
     https://api.remot3.it/apv/v27/device/connect | jq -r '.connection.proxy')
     firefox $WEB
+    elif [ "$1" == "localweb" ] ; then
+    firefox $LOCALIP
+    elif [ "$1" == "localsftp" ] ; then
+    thunar "sftp://$LOCALIP"
+    elif [ "$1" == "remotesftp" ] ; then
+    initialize_token
+    initialize_proxyaddress
+    thunar "sftp://${PROXYADDRESS[0]}:${PROXYADDRESS[1]}"
 fi
